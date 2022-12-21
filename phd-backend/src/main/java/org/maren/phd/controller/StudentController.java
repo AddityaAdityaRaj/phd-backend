4c9a83ab24b0cc6d26a80065b7b75981a3ca1554
MayankPrajapati08 <mayankprajapati0810@gmail.com>
2022-12-21 23:41:43 +0530

    Added controller

--- phd-backend/src/main/java/org/maren/phd/controller/StudentController.java
+++ phd-backend/src/main/java/org/maren/phd/controller/StudentController.java
@@ -1,16 +1,33 @@
 package org.maren.phd.controller;
 
-import java.util.List;
-
 import org.maren.phd.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.maren.phd.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
 
+	@Autowired
+	private StudentService studentService;
+
+	// Create
+	@PostMapping("/student")
+	public Student createStudent(
+			@RequestBody Student student) {
+		return studentService.createStudent(student);
+	}
+
 	@GetMapping("/student")
-	public List<Student> getStudentById(long id){
-		return null;
+	public List<Student> fetchStudent() {
+		return studentService.fetchStudent();
+	}
+
+	@GetMapping("/student{id}")
+	public Optional <Student> fetchStudent(@PathVariable("id") Long studentId) {
+		return studentService.fetchStudent(studentId);
 	}
 }
