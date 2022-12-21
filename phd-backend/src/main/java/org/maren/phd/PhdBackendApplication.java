75bd91dee8bd6d926e356418c9bdcd79e336e824
Rakesh Kymar Ray <rakeshrayk@gmail.com>
2022-12-21 21:48:03 +0530

    1st push
    1st push

---
+++ phd-backend/src/main/java/org/maren/phd/PhdBackendApplication.java
@@ -0,0 +1,13 @@
+package org.maren.phd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhdBackendApplication.class, args);
+	}
+
+}
---
+++ phd-backend/src/test/java/org/maren/phd/PhdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maren.phd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
